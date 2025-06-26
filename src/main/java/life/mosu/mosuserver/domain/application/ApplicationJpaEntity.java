ef5eaa37587f5e581339a728197aaf34fc5e79fe
chominju <cmj021002@knu.ac.kr>
2025-06-26 18:07:55 +0900

    MOSU-1 feat: Application Entity 구현

---
+++ src/main/java/life/mosu/mosuserver/domain/application/ApplicationJpaEntity.java
@@ -0,0 +1,70 @@
+package life.mosu.mosuserver.domain.application;
+
+import jakarta.persistence.*;
+import life.mosu.mosuserver.domain.base.BaseTimeEntity;
+import life.mosu.mosuserver.infra.storage.domain.AdmissionTicketImage;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "application")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApplicationJpaEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_id")
+    private int Id;
+
+    @Column(name ="user_id")
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private Lunch lunch;
+
+    @Column(name = "examination")
+    private String examinationNumber;
+
+    @Embedded
+    private AdmissionTicketImage admissionTicketImage;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "exam_subject", joinColumns = @JoinColumn(name = "application_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Subject> subjects = new HashSet<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "application_school", joinColumns = @JoinColumn(name = "application_id"))
+    @Column(name = "school_id", nullable = false)
+    private Set<Long> schoolIds = new HashSet<>();
+
+    @Builder
+    public ApplicationJpaEntity(
+        final Long userId,
+        final Lunch lunch,
+        final String examinationNumber,
+        final AdmissionTicketImage admissionTicketImage,
+        final Set<Subject> subjects,
+        final Set<Long> schoolIds
+    ) {
+        this.userId = userId;
+        this.lunch = lunch;
+        this.examinationNumber = examinationNumber;
+        this.admissionTicketImage = admissionTicketImage;
+
+        if (subjects == null || subjects.size() != 5) {
+            throw new IllegalArgumentException("과목은 5개를 선택해야 합니다.");
+        }
+        this.subjects = subjects;
+
+        if (schoolIds == null || schoolIds.isEmpty()) {
+            throw new IllegalArgumentException("최소 한 개 이상의 학교를 선택해야 합니다.");
+        }
+        this.schoolIds = schoolIds;
+    }
+
+}
