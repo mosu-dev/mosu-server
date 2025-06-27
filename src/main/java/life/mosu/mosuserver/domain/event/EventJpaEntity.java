1e8c6e9b96ca01e102cd710a88c9c339e8efe75c
wlgns12370 <knu12370@knu.ac.kr>
2025-06-27 17:54:20 +0900

    MOSU-2 feat: Event Entity 구현

---
+++ src/main/java/life/mosu/mosuserver/domain/event/EventJpaEntity.java
@@ -0,0 +1,44 @@
+package life.mosu.mosuserver.domain.event;
+
+import jakarta.persistence.*;
+import life.mosu.mosuserver.domain.base.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "event")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EventJpaEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id", nullable = false)
+    private Long id;
+
+    @Column(name = "event_description", nullable = false)
+    private String eventDescription;
+
+    @Column(name = "event_link")
+    private String eventLink;
+
+    @Column(name = "event_img")
+    private String eventImg;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Builder
+    public EventJpaEntity(
+        final String eventDescription,
+        final String eventLink,
+        final String eventImg,
+        final Long userId
+    ) {
+        this.eventDescription = eventDescription;
+        this.eventLink = eventLink;
+        this.eventImg = eventImg;
+        this.userId = userId;
+    }
+}
