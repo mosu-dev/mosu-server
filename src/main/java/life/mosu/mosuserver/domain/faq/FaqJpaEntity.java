fdd543ffb90a39f6242b4139cb65bfc65677e19c
wlgns12370 <knu12370@knu.ac.kr>
2025-06-27 17:54:02 +0900

    MOSU-2 feat: Faq Entity 구현

---
+++ src/main/java/life/mosu/mosuserver/domain/faq/FaqJpaEntity.java
@@ -0,0 +1,34 @@
+package life.mosu.mosuserver.domain.faq;
+
+import jakarta.persistence.*;
+import life.mosu.mosuserver.domain.base.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "faq")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class FaqJpaEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "faq_id", nullable = false)
+    private Long id;
+
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @Column(name = "answer", nullable = false)
+    private String answer;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Builder
+    public FaqJpaEntity(final String question, final String answer, final Long userId) {
+        this.question = question;
+        this.answer = answer;
+        this.userId = userId;
+    }
+}
