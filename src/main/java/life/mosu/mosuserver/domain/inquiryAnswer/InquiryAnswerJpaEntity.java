73b0150e1afe8e034822a46ea5b6238a440298ae
wlgns12370 <knu12370@knu.ac.kr>
2025-06-27 17:51:16 +0900

    MOSU-2 feat: InquiryAnswer Entity 구현

---
+++ src/main/java/life/mosu/mosuserver/domain/inquiryAnswer/InquiryAnswerJpaEntity.java
@@ -0,0 +1,39 @@
+package life.mosu.mosuserver.domain.inquiryAnswer;
+
+import jakarta.persistence.*;
+import life.mosu.mosuserver.domain.base.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "inquiry_answer")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InquiryAnswerJpaEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inquiry_answer_id", nullable = false)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "inquiry_id", nullable = false)
+    private Long inquiryId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Builder
+    public InquiryAnswerJpaEntity(
+        final String content,
+        final Long inquiryId,
+        final Long userId
+    ) {
+        this.content = content;
+        this.inquiryId = inquiryId;
+        this.userId = userId;
+    }
+}
