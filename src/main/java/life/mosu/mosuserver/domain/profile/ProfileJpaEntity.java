3251abd982b15bde7eda414bca77b1749ca66bcd
chominju <cmj021002@knu.ac.kr>
2025-07-01 16:25:04 +0900

    MOSU-6 feat: ProfileJpaEntity column nullable 옵션, builder, edit 함수 추가

--- src/main/java/life/mosu/mosuserver/domain/profile/ProfileJpaEntity.java
+++ src/main/java/life/mosu/mosuserver/domain/profile/ProfileJpaEntity.java
@@ -2,7 +2,9 @@ package life.mosu.mosuserver.domain.profile;
 
 import jakarta.persistence.*;
 import life.mosu.mosuserver.domain.base.BaseTimeEntity;
+import life.mosu.mosuserver.presentation.profile.dto.EditProfileRequest;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +12,8 @@ import java.time.LocalDate;
 
 @Entity
 @Getter
-@Table(name = "profile")
+@Table(name = "profile",
+    uniqueConstraints = @UniqueConstraint(columnNames = "user_id"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProfileJpaEntity extends BaseTimeEntity {
 
@@ -19,19 +22,20 @@ public class ProfileJpaEntity extends BaseTimeEntity {
     @Column(name = "profile_id")
     private Long id;
 
-    @Column(name="user_id")
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
-    @Column(name = "user_name")
+    @Column(name = "user_name", nullable = false)
     private String userName;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @Column(name = "birth")
+    @Column(name = "birth", nullable = false)
     private LocalDate birth;
 
-    @Column(name = "phone_number")
+    @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
     @Column(name = "email")
@@ -45,5 +49,39 @@ public class ProfileJpaEntity extends BaseTimeEntity {
 
     @Enumerated(EnumType.STRING)
     private Grade grade;
+
+    @Builder
+    public ProfileJpaEntity(
+        final Long userId,
+        final String userName,
+        final Gender gender,
+        final LocalDate birth,
+        final String phoneNumber,
+        final String email,
+        final Education education,
+        final SchoolInfoJpaVO schoolInfo,
+        final Grade grade
+    ) {
+        this.userId = userId;
+        this.userName = userName;
+        this.gender = gender;
+        this.birth = birth;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.education = education;
+        this.schoolInfo = schoolInfo;
+        this.grade = grade;
+    }
+
+    public void edit(final EditProfileRequest request) {
+        this.userName = request.userName();
+        this.gender = request.gender();
+        this.birth = request.birth();
+        this.phoneNumber = request.phoneNumber();
+        this.email = request.email();
+        this.education = request.education();
+        this.schoolInfo = request.schoolInfo().toEntity();
+        this.grade = request.grade();
+    }
 
 }
