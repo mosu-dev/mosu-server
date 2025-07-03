62a6abccd6a759c7a6aaa7ac6a07ce4437dc8dbc
KNU-K <knukang334@gmail.com>
2025-07-03 22:35:01 +0900

    feat: BaseFakeRestOperationsStub, ConfirmFakeRestOperationsStub, CancelFakeRestOperationsStub 클래스 추가 - 결제 확인 및 취소 요청을 위한 스텁 구현

---
+++ src/test/java/life/mosu/mosuserver/payment/stub/ConfirmFakeRestOperationsStub.java
@@ -0,0 +1,28 @@
+package life.mosu.mosuserver.payment.stub;
+
+import life.mosu.mosuserver.infra.payment.application.dto.ConfirmTossPaymentResponse;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+public class ConfirmFakeRestOperationsStub extends BaseFakeRestOperationsStub {
+    private final ConfirmTossPaymentResponse response;
+
+    public ConfirmFakeRestOperationsStub(ConfirmTossPaymentResponse response) {
+        this.response = response;
+    }
+
+    @Override
+    public <T> ResponseEntity<T> exchange(String url,
+            HttpMethod method,
+            HttpEntity<?> requestEntity,
+            Class<T> responseType,
+            Object... uriVariables) {
+
+        if (!url.contains("/confirm")) {
+            throw new IllegalArgumentException("ConfirmFakeRestOperations can only handle /confirm");
+        }
+        return ResponseEntity.ok(responseType.cast(response));
+    }
+}
+
