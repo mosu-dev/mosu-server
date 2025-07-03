5228bda1fd68ad99b2aa1698de088f749eb03c38
KNU-K <knukang334@gmail.com>
2025-07-03 22:32:01 +0900

    feat: PaymentJpaEntity 수정 - id 타입 변경 및 생성자 매개변수 추가

--- src/main/java/life/mosu/mosuserver/domain/payment/PaymentJpaEntity.java
+++ src/main/java/life/mosu/mosuserver/domain/payment/PaymentJpaEntity.java
@@ -9,34 +9,67 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name="payment")
+@Table(name = "payment")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PaymentJpaEntity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "payment_id")
-    private int id;
+    private Long id;
 
     @Column(name = "application_id")
     private Long applicationId;
 
+    @Column(name = "payment_key", nullable = false)
+    private String paymentKey;
+
+    @Column(name = "order_id", nullable = false)
+    private String orderId;
+
+    @Embedded
+    private PaymentAmountVO paymentAmount;
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
     private PaymentStatus paymentStatus;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "method", nullable = false)
     private PaymentMethod paymentMethod;
 
-    @Builder
-    public PaymentJpaEntity(
-        final Long applicationId,
-        final PaymentStatus paymentStatus,
-        final PaymentMethod paymentMethod
+    @Builder(access = AccessLevel.PRIVATE)
+    private PaymentJpaEntity(
+            Long applicationId,
+            String paymentKey,
+            String orderId,
+            PaymentAmountVO paymentAmount,
+            PaymentStatus paymentStatus,
+            PaymentMethod paymentMethod
     ) {
         this.applicationId = applicationId;
+        this.paymentKey = paymentKey;
+        this.orderId = orderId;
+        this.paymentAmount = paymentAmount;
         this.paymentStatus = paymentStatus;
         this.paymentMethod = paymentMethod;
     }
 
-
+    public static PaymentJpaEntity of(
+            Long applicationId,
+            String paymentKey,
+            String orderId,
+            PaymentStatus paymentStatus,
+            PaymentAmountVO paymentAmount,
+            PaymentMethod paymentMethod
+    ) {
+        return PaymentJpaEntity.builder()
+                .applicationId(applicationId)
+                .paymentKey(paymentKey)
+                .orderId(orderId)
+                .paymentStatus(paymentStatus)
+                .paymentAmount(paymentAmount)
+                .paymentMethod(paymentMethod)
+                .build();
+    }
 }
