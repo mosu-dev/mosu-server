f48ebc8ab9cd94c243f596141fa639d31c468a88
KNU-K <knukang334@gmail.com>
2025-07-03 22:33:18 +0900

    feat: ObjectMapperConfig 클래스 추가 - Jackson ObjectMapper 빈 설정을 위한 구성 클래스 구현

---
+++ src/main/java/life/mosu/mosuserver/global/config/ObjectMapperConfig.java
@@ -0,0 +1,13 @@
+package life.mosu.mosuserver.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+    @Bean
+    ObjectMapper mapper(){
+        return new ObjectMapper();
+    }
+}
