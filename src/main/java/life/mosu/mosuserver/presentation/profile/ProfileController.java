a4d4cc59552421e57f84350c3743b0596aacb268
wlgns12370 <knu12370@knu.ac.kr>
2025-07-06 04:16:24 +0900

    MOSU-40 feat: Profile User id 적용

--- src/main/java/life/mosu/mosuserver/presentation/profile/ProfileController.java
+++ src/main/java/life/mosu/mosuserver/presentation/profile/ProfileController.java
@@ -2,6 +2,7 @@ package life.mosu.mosuserver.presentation.profile;
 
 import jakarta.validation.Valid;
 import life.mosu.mosuserver.application.profile.ProfileService;
+import life.mosu.mosuserver.global.annotation.UserId;
 import life.mosu.mosuserver.global.util.ApiResponseWrapper;
 import life.mosu.mosuserver.presentation.profile.dto.EditProfileRequest;
 import life.mosu.mosuserver.presentation.profile.dto.ProfileDetailResponse;
@@ -9,28 +10,37 @@ import life.mosu.mosuserver.presentation.profile.dto.ProfileRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/profile")
 public class ProfileController {
+
     private final ProfileService profileService;
 
     @PostMapping
     public ApiResponseWrapper<Void> create(
-        @RequestParam Long userId,
-        @Valid @RequestBody ProfileRequest request
+            @UserId Long userId,
+            @Valid @RequestBody ProfileRequest request
     ) {
         profileService.registerProfile(userId, request);
         return ApiResponseWrapper.success(HttpStatus.CREATED, "프로필 등록 성공");
     }
 
     @PutMapping
+    @PreAuthorize("isAuthenticated() and hasRole('USER')")
     public ApiResponseWrapper<Void> update(
-        @RequestParam Long userId,
-        @Valid @RequestBody EditProfileRequest request
+            @UserId Long userId,
+            @Valid @RequestBody EditProfileRequest request
     ) {
         profileService.editProfile(userId, request);
         return ApiResponseWrapper.success(HttpStatus.OK, "프로필 수정 성공");
@@ -38,7 +48,7 @@ public class ProfileController {
 
     @GetMapping
     public ApiResponseWrapper<ProfileDetailResponse> getProfile(
-        @RequestParam Long userId) {
+            @RequestParam Long userId) {
         ProfileDetailResponse response = profileService.getProfile(userId);
         return ApiResponseWrapper.success(HttpStatus.OK, "프로필 조회 성공", response);
     }
