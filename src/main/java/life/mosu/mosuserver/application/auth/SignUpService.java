c0f5cb9f3ae056fa2b34c96c92db4be377951598
wlgns12370 <knu12370@knu.ac.kr>
2025-07-06 05:23:45 +0900

    MOSU-40 fix: @Transactional 제거

--- src/main/java/life/mosu/mosuserver/application/auth/SignUpService.java
+++ src/main/java/life/mosu/mosuserver/application/auth/SignUpService.java
@@ -8,11 +8,9 @@ import life.mosu.mosuserver.presentation.auth.dto.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 @RequiredArgsConstructor
 public class SignUpService {
 
