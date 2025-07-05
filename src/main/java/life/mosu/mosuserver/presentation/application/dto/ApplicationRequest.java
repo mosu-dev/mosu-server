1805251d281d6dde9214bddac977678a75743663
chominju <cmj021002@knu.ac.kr>
2025-07-06 01:09:31 +0900

    MOSU-19 feat: 신청 관련 request dto 구현

--- src/main/java/life/mosu/mosuserver/presentation/application/dto/ApplicationRequest.java
+++ src/main/java/life/mosu/mosuserver/presentation/application/dto/ApplicationRequest.java
@@ -1,15 +1,23 @@
 package life.mosu.mosuserver.presentation.application.dto;
 
 import jakarta.validation.constraints.NotNull;
+import life.mosu.mosuserver.domain.application.ApplicationJpaEntity;
 import life.mosu.mosuserver.presentation.faq.dto.FileRequest;
 
 import java.util.Set;
 
 public record ApplicationRequest(
     FileRequest admissionTicket,
-    @NotNull String recommenderPhoneNumber,
+    String recommenderPhoneNumber,
     @NotNull Set<ApplicationSchoolRequest> schools,
-    @NotNull AgreementRequest agreementRequest, //이 부분 어떻게 저장하면 좋을지
-    @NotNull Integer amount
+    @NotNull AgreementRequest agreementRequest
 ) {
+    public ApplicationJpaEntity toEntity(Long userId) {
+        return ApplicationJpaEntity.builder()
+            .userId(userId)
+            .recommenderPhoneNumber(recommenderPhoneNumber)
+            .agreedToNotices(agreementRequest().agreedToNotices())
+            .agreedToRefundPolicy(agreementRequest().agreedToRefundPolicy())
+            .build();
+    }
 }
--- src/main/java/life/mosu/mosuserver/presentation/application/dto/ApplicationSchoolRequest.java
+++ src/main/java/life/mosu/mosuserver/presentation/application/dto/ApplicationSchoolRequest.java
@@ -4,32 +4,60 @@ import life.mosu.mosuserver.domain.application.Lunch;
 import life.mosu.mosuserver.domain.application.Subject;
 import life.mosu.mosuserver.domain.applicationschool.ApplicationSchoolJpaEntity;
 import life.mosu.mosuserver.domain.school.Area;
+import life.mosu.mosuserver.global.exception.CustomRuntimeException;
+import life.mosu.mosuserver.global.exception.ErrorCode;
 
 import java.time.LocalDate;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public record ApplicationSchoolRequest(
     Long schoolId,
     String schoolName,
-    Area area,
+    String area,
     AddressRequest address,
     LocalDate examDate,
-    Lunch lunch,
-    Set<Subject> subjects,
-    Integer amount
+    String lunch,
+    Set<String> subjects
 ) {
 
-    public ApplicationSchoolJpaEntity toEntity(Long applicationId) {
+    public ApplicationSchoolJpaEntity toEntity(Long userId, Long applicationId) {
         return ApplicationSchoolJpaEntity.builder()
+            .userId(userId)
             .applicationId(applicationId)
             .schoolId(schoolId)
             .schoolName(schoolName)
-            .area(area)
+            .area(validatedArea(area))
             .address(address.toValueObject())
             .examDate(examDate)
-            .lunch(lunch)
-            .subjects(subjects)
-            .amount(amount)
+            .lunch(validatedLunch(lunch))
+            .subjects(validatedSubjects(subjects))
             .build();
+    }
+
+    private Set<Subject> validatedSubjects(Set<String> subjects) {
+        try {
+            return subjects.stream()
+                .map(Subject::valueOf)
+                .collect(Collectors.toSet());
+        } catch (IllegalArgumentException e) {
+            throw new CustomRuntimeException(ErrorCode.WRONG_SUBJECT_TYPE);
+        }
+    }
+
+    private Lunch validatedLunch(String lunch) {
+        try {
+            return Lunch.valueOf(lunch.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new CustomRuntimeException(ErrorCode.WRONG_LUNCH_TYPE);
+        }
+    }
+
+    private Area validatedArea(String area) {
+        try {
+            return Area.valueOf(area.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new CustomRuntimeException(ErrorCode.WRONG_AREA_TYPE);
+        }
     }
 }
