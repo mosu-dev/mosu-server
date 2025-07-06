8140c04590cffe10d65d1cae7bbdcfca51b82ba7
KNU-K <knukang334@gmail.com>
2025-07-07 02:19:16 +0900

    feat: PaymentRepository 인터페이스에 코드 정리 및 공백 추가

--- src/main/java/life/mosu/mosuserver/domain/payment/PaymentRepository.java
+++ src/main/java/life/mosu/mosuserver/domain/payment/PaymentRepository.java
@@ -1,8 +1,12 @@
 package life.mosu.mosuserver.domain.payment;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<PaymentJpaEntity, Long>{
+public interface PaymentRepository extends JpaRepository<PaymentJpaEntity, Long> {
+
     // TODO:인덱스 처리 필요(풀스캔 위험)
     boolean existsByOrderId(String orderId);
+
+    List<PaymentJpaEntity> findByOrderId(String orderId);
 }
