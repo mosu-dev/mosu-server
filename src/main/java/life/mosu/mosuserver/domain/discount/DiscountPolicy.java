040baf8fe50e1767731dd7f84893aba21140ae4d
KNU-K <knukang334@gmail.com>
2025-07-07 02:18:46 +0900

    feat: DiscountPolicy에 정적 메서드 calculate 추가

--- src/main/java/life/mosu/mosuserver/domain/discount/DiscountPolicy.java
+++ src/main/java/life/mosu/mosuserver/domain/discount/DiscountPolicy.java
@@ -4,9 +4,13 @@ public enum DiscountPolicy {
     QUANTITY_PERCENTAGE(new QuantityPercentageDiscountCalculator()),
     FIXED_QUANTITY(new FixedQuantityDiscountCalculator());
     private final DiscountCalculator calculator;
-    
+
     DiscountPolicy(DiscountCalculator calculator) {
         this.calculator = calculator;
+    }
+
+    public static int calculate(DiscountPolicy discountPolicy, int applicationCount) {
+        return discountPolicy.calculateDiscount(applicationCount);
     }
 
     public int calculateDiscount(int quantity) {
