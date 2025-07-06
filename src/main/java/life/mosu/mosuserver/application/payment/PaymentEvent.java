ad233df777b911aaa4482b003634db494ce50cfa
KNU-K <knukang334@gmail.com>
2025-07-07 02:18:17 +0900

    feat: PaymentEvent에 applicationIds 및 totalAmount 필드 추가

--- src/main/java/life/mosu/mosuserver/application/payment/PaymentEvent.java
+++ src/main/java/life/mosu/mosuserver/application/payment/PaymentEvent.java
@@ -1,19 +1,28 @@
 package life.mosu.mosuserver.application.payment;
 
+import java.util.List;
 import life.mosu.mosuserver.domain.payment.PaymentStatus;
 
-public record PaymentEvent(String applicationId, String orderId, PaymentStatus status) {
+public record PaymentEvent(
+        List<Long> applicationIds,
+        String orderId,
+        PaymentStatus status,
+        Integer totalAmount
+) {
 
-    public static PaymentEvent ofSuccess(String applicationId, String orderId) {
-        return new PaymentEvent(applicationId, orderId, PaymentStatus.DONE);
+    public static PaymentEvent ofSuccess(List<Long> applicationIds, String orderId,
+            Integer totalAmount) {
+        return new PaymentEvent(applicationIds, orderId, PaymentStatus.DONE, totalAmount);
     }
 
-    public static PaymentEvent ofCancelled(String applicationId, String orderId) {
-        return new PaymentEvent(applicationId, orderId, PaymentStatus.CANCELLED_DONE);
+    public static PaymentEvent ofCancelled(List<Long> applicationIds, String orderId,
+            Integer totalAmount) {
+        return new PaymentEvent(applicationIds, orderId, PaymentStatus.CANCELLED_DONE, totalAmount);
     }
 
-    public static PaymentEvent ofFailed(String applicationId, String orderId) {
-        return new PaymentEvent(applicationId, orderId, PaymentStatus.ABORTED);
+    public static PaymentEvent ofFailed(List<Long> applicationIds, String orderId,
+            Integer totalAmount) {
+        return new PaymentEvent(applicationIds, orderId, PaymentStatus.ABORTED, totalAmount);
     }
 
     @Override
