3f74065c65a988bafe9be98dc000cd10566dd6a9
KNU-K <knukang334@gmail.com>
2025-07-06 20:11:49 +0900

    feat: PaymentRepository에 orderId 존재 여부 확인 메서드 추가

--- src/main/java/life/mosu/mosuserver/domain/payment/PaymentRepository.java
+++ src/main/java/life/mosu/mosuserver/domain/payment/PaymentRepository.java
@@ -3,5 +3,6 @@ package life.mosu.mosuserver.domain.payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<PaymentJpaEntity, Long>{
-
+    // TODO:인덱스 처리 필요(풀스캔 위험)
+    boolean existsByOrderId(String orderId);
 }
