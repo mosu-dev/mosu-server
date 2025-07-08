a21f3b0bc290ca99da1c31f318e7003feb28e77e
chominju <cmj021002@knu.ac.kr>
2025-07-09 00:51:01 +0900

    MOSU-39 chore: @UserId 적용

--- src/main/java/life/mosu/mosuserver/presentation/application/ApplicationController.java
+++ src/main/java/life/mosu/mosuserver/presentation/application/ApplicationController.java
@@ -1,16 +1,21 @@
 package life.mosu.mosuserver.presentation.application;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import life.mosu.mosuserver.application.application.ApplicationService;
+import life.mosu.mosuserver.global.annotation.UserId;
 import life.mosu.mosuserver.global.util.ApiResponseWrapper;
 import life.mosu.mosuserver.presentation.application.dto.ApplicationRequest;
 import life.mosu.mosuserver.presentation.application.dto.ApplicationResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -22,9 +27,10 @@ public class ApplicationController {
 
     //신청
     @PostMapping
+    @PreAuthorize("isAuthenticated() and hasRole('USER')")
     public ApiResponseWrapper<ApplicationResponse> apply(
-        @RequestParam Long userId,
-        @Valid @RequestBody ApplicationRequest request
+            @UserId Long userId,
+            @Valid @RequestBody ApplicationRequest request
     ) {
         ApplicationResponse response = applicationService.apply(userId, request);
         return ApiResponseWrapper.success(HttpStatus.OK, "신청 성공", response);
@@ -32,8 +38,8 @@ public class ApplicationController {
 
     //전체 신청 내역 조회
     @GetMapping
-    public ApiResponseWrapper<List<ApplicationResponse>> getAll(
-        @RequestParam Long userId
+    public ApiResponseWrapper<List<ApplicationResponse>> getApplications(
+            @UserId Long userId
     ) {
         List<ApplicationResponse> responses = applicationService.getApplications(userId);
         return ApiResponseWrapper.success(HttpStatus.OK, "신청 내역 조회 성공", responses);
