6cd51f064a378c3b57cee792dd38aa659411e581
Cho Minju <120126604+chominju02@users.noreply.github.com>
2025-07-10 02:03:39 +0900

    MOSU-62 feat: application request, reponse 타입 변경 (#64)
    * MOSU-62 feat: 신청 시 학교 검증 로직 추가 및 서비스 수정
    
    * MOSU-62 remove: ApplicationSchoolRequest schoolId 삭제
    
    * MOSU-62 chore: ApplicationSchoolResponse 값으로 반환
    
    * MOSU-62 chore: area 수정

--- src/main/java/life/mosu/mosuserver/application/application/ApplicationService.java
+++ src/main/java/life/mosu/mosuserver/application/application/ApplicationService.java
@@ -3,14 +3,12 @@ package life.mosu.mosuserver.application.application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import life.mosu.mosuserver.domain.application.AdmissionTicketImageJpaEntity;
 import life.mosu.mosuserver.domain.application.AdmissionTicketImageJpaRepository;
 import life.mosu.mosuserver.domain.application.ApplicationJpaEntity;
 import life.mosu.mosuserver.domain.application.ApplicationJpaRepository;
 import life.mosu.mosuserver.domain.application.ApplicationSchoolJpaRepository;
 import life.mosu.mosuserver.domain.applicationschool.ApplicationSchoolJpaEntity;
-import life.mosu.mosuserver.domain.school.AddressJpaVO;
 import life.mosu.mosuserver.domain.school.SchoolJpaEntity;
 import life.mosu.mosuserver.domain.school.SchoolJpaRepository;
 import life.mosu.mosuserver.global.exception.CustomRuntimeException;
@@ -38,38 +36,36 @@ public class ApplicationService {
     // 신청
     @Transactional
     public ApplicationResponse apply(Long userId, ApplicationRequest request) {
-        Set<ApplicationSchoolRequest> schools = request.schools();
-        List<ApplicationSchoolJpaEntity> schoolEntities = new ArrayList<>();
+        Set<ApplicationSchoolRequest> schoolRequests = request.schools();
+        List<ApplicationSchoolJpaEntity> savedEntities = new ArrayList<>();
 
-        Set<Long> schoolIds = schools.stream()
-                .map(ApplicationSchoolRequest::schoolId)
-                .collect(Collectors.toSet());
-
-        if (applicationSchoolJpaRepository.existsByUserIdAndSchoolIdIn(userId, schoolIds)) {
-            throw new CustomRuntimeException(ErrorCode.APPLICATION_SCHOOL_ALREADY_APPLIED);
-        }
-
-        ApplicationJpaEntity application = request.toEntity(userId);
-        ApplicationJpaEntity applicationJpaEntity = applicationJpaRepository.save(application);
-        Long applicationId = applicationJpaEntity.getId();
+        ApplicationJpaEntity application = applicationJpaRepository.save(request.toEntity(userId));
+        Long applicationId = application.getId();
 
         admissionTicketImageJpaRepository.save(
                 createAdmissionTicketImageIfPresent(request.admissionTicket(), applicationId));
 
-        schools.forEach(applicationSchoolRequest -> {
-            SchoolJpaEntity school = schoolJpaRepository.findById(
-                            applicationSchoolRequest.schoolId())
+        for (ApplicationSchoolRequest schoolRequest : schoolRequests) {
+            Long schoolId = schoolJpaRepository.findBySchoolNameAndAreaAndExamDate(
+                            schoolRequest.schoolName(),
+                            schoolRequest.validatedArea(schoolRequest.area()),
+                            schoolRequest.examDate())
+                    .orElseThrow(() -> new CustomRuntimeException(ErrorCode.SCHOOL_NOT_FOUND))
+                    .getId();
+
+            if (applicationSchoolJpaRepository.existsByUserIdAndSchoolId(userId, schoolId)) {
+                throw new CustomRuntimeException(ErrorCode.APPLICATION_SCHOOL_ALREADY_APPLIED);
+            }
+
+            SchoolJpaEntity school = schoolJpaRepository.findById(schoolId)
                     .orElseThrow(() -> new CustomRuntimeException(ErrorCode.SCHOOL_NOT_FOUND));
 
-            AddressJpaVO address = school.getAddress();
-            ApplicationSchoolJpaEntity applicationSchoolJpaEntity = applicationSchoolRequest.toEntity(
-                    userId, applicationId, address);
-            ApplicationSchoolJpaEntity saved = applicationSchoolJpaRepository.save(
-                    applicationSchoolJpaEntity);
-            schoolEntities.add(saved);
-        });
+            ApplicationSchoolJpaEntity applicationSchool = schoolRequest.toEntity(userId,
+                    applicationId, school);
+            savedEntities.add(applicationSchoolJpaRepository.save(applicationSchool));
+        }
 
-        return ApplicationResponse.of(applicationId, schoolEntities);
+        return ApplicationResponse.of(applicationId, savedEntities);
     }
 
 
