a4b6525cf790608869cca06cf73a9c1c2bea3512
chominju <cmj021002@knu.ac.kr>
2025-07-10 05:55:49 +0900

    MOSU-67 feat: admin 관련 swagger 적용

--- src/main/java/life/mosu/mosuserver/presentation/admin/dto/StudentFilter.java
+++ src/main/java/life/mosu/mosuserver/presentation/admin/dto/StudentFilter.java
@@ -1,11 +1,21 @@
 package life.mosu.mosuserver.presentation.admin.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import life.mosu.mosuserver.global.annotation.PhoneNumberPattern;
 
+@Schema(description = "학생 목록 필터 DTO")
 public record StudentFilter(
+
+        @Schema(description = "이름 필터", example = "홍길동")
         String name,
-        @PhoneNumberPattern String phone,
+
+        @Schema(description = "전화번호 필터", example = "01012345678")
+        @PhoneNumberPattern
+        String phone,
+
+        @Schema(description = "정렬 순서 (desc 또는 asc)", example = "desc", defaultValue = "desc")
         String order
+
 ) {
 
     public StudentFilter {
