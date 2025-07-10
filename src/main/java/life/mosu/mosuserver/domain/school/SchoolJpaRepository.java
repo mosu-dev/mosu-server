9e43c71fdd3d08e7ed31861400f7f8792b4f7886
wlgns12370 <knu12370@knu.ac.kr>
2025-07-11 00:52:37 +0900

    MOSU-77 feat: SchoolRepository 개발

--- src/main/java/life/mosu/mosuserver/domain/school/SchoolJpaRepository.java
+++ src/main/java/life/mosu/mosuserver/domain/school/SchoolJpaRepository.java
@@ -1,10 +1,20 @@
 package life.mosu.mosuserver.domain.school;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SchoolJpaRepository extends JpaRepository<SchoolJpaEntity, Long> {
+
+    @Query("""
+                SELECT new life.mosu.mosuserver.domain.school.SchoolApplicationProjection(s.id, s.schoolName, COUNT(a))
+                FROM SchoolJpaEntity s
+                LEFT JOIN ApplicationSchoolJpaEntity a ON a.schoolId = s.id
+                GROUP BY s.id, s.schoolName
+            """)
+    List<SchoolApplicationProjection> countBySchoolNameGroupBy();
 
     Optional<SchoolJpaEntity> findBySchoolNameAndAreaAndExamDate(String schoolName, Area area,
             LocalDate examDate);
