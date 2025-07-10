dc5689ab3a4fa91afbdd6de344504499869fbe0a
wlgns12370 <knu12370@knu.ac.kr>
2025-07-10 21:38:37 +0900

    MOSU-69 feat: 추천인 Service 구현

---
+++ src/main/java/life/mosu/mosuserver/application/profile/RecommenderService.java
@@ -0,0 +1,39 @@
+package life.mosu.mosuserver.application.profile;
+
+import life.mosu.mosuserver.domain.profile.ProfileJpaEntity;
+import life.mosu.mosuserver.domain.profile.ProfileJpaRepository;
+import life.mosu.mosuserver.global.exception.CustomRuntimeException;
+import life.mosu.mosuserver.global.exception.ErrorCode;
+import life.mosu.mosuserver.presentation.profile.dto.RecommenderRegistrationRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RecommenderService {
+
+    private final ProfileJpaRepository profileJpaRepository;
+
+    @Transactional
+    public void registerRecommender(Long userId, RecommenderRegistrationRequest request) {
+        ProfileJpaEntity profile = profileJpaRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomRuntimeException(ErrorCode.PROFILE_NOT_FOUND));
+        if (profile.getRecommenderPhoneNumber() != null) {
+            throw new CustomRuntimeException(ErrorCode.AREADY_REGISTERED_RECOMMENDER);
+        }
+
+        profile.registerRecommenderPhoneNumber(request.phoneNumber());
+    }
+
+    @Transactional(readOnly = true, propagation = Propagation.NOT_SUPPORTED)
+    public Boolean verifyRecommender(Long userId) {
+        ProfileJpaEntity profile = profileJpaRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomRuntimeException(ErrorCode.PROFILE_NOT_FOUND));
+        return profile.getRecommenderPhoneNumber() != null;
+    }
+
+}
