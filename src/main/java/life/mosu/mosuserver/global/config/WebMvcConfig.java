b5c097ccd2acb771cba3b770d9677ffb422ad169
wlgns12370 <knu12370@knu.ac.kr>
2025-07-10 21:38:00 +0900

    MOSU-69 feat: API 연동을 위한 CORS 해제

--- src/main/java/life/mosu/mosuserver/global/config/WebMvcConfig.java
+++ src/main/java/life/mosu/mosuserver/global/config/WebMvcConfig.java
@@ -1,20 +1,32 @@
 package life.mosu.mosuserver.global.config;
 
+import java.util.List;
 import life.mosu.mosuserver.global.resolver.UserIdArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
     private final UserIdArgumentResolver resolver;
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(resolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080",
+                        "http://api.mosuedu.com", "https://api.mosuedu.com")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
