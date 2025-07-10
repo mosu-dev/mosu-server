1dde35c79705f02715a6630e6065846f5deb151e
KNU-K <knukang334@gmail.com>
2025-07-10 16:33:01 +0900

    refactor: existsByUserIdAndSchoolId 메서드 시그니처 수정 및 불필요한 임포트 제거

--- src/main/java/life/mosu/mosuserver/domain/applicationschool/ApplicationSchoolJpaRepository.java
+++ src/main/java/life/mosu/mosuserver/domain/applicationschool/ApplicationSchoolJpaRepository.java
@@ -1,16 +1,21 @@
 package life.mosu.mosuserver.domain.applicationschool;
 
-import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApplicationSchoolJpaRepository extends
         JpaRepository<ApplicationSchoolJpaEntity, Long> {
 
 
-    boolean existsByUserIdAndSchoolIdIn(Long userId, Collection<Long> schoolIds);
+    boolean existsByUserIdAndSchoolId(Long userId, Long schoolId);
 
     List<ApplicationSchoolJpaEntity> findAllByApplicationId(Long applicationId);
 
     boolean existsByApplicationId(Long applicationId);
+
+    @Query("SELECT COUNT(a) = :size FROM ApplicationSchoolJpaEntity a WHERE a.id IN :applicationSchoolIds")
+    boolean existsAllByIds(@Param("applicationSchoolIds") List<Long> applicationSchoolIds,
+            @Param("size") long size);
 }
