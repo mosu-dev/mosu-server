7190c9aac8c5114809a6385278bdb7e3a4dea6bb
KNU-K <knukang334@gmail.com>
2025-07-10 16:25:57 +0900

    refactor: ApplicationSchoolJpaRepository 삭제 및 관련 쿼리 제거

--- src/main/java/life/mosu/mosuserver/domain/application/ApplicationJpaRepository.java
+++ src/main/java/life/mosu/mosuserver/domain/application/ApplicationJpaRepository.java
@@ -2,14 +2,8 @@ package life.mosu.mosuserver.domain.application;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ApplicationJpaRepository extends JpaRepository<ApplicationJpaEntity, Long> {
 
-    @Query("SELECT COUNT(a) = :size FROM ApplicationJpaEntity a WHERE a.id IN :applicationIds")
-    boolean existsAllByIds(@Param("applicationIds") List<Long> applicationIds,
-            @Param("size") long size);
-  
     List<ApplicationJpaEntity> findAllByUserId(Long userId);
 }
